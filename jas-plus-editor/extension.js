d7e282962eb7b4faa2e272fbd5dfb495c1f0b3d5
Amauri Silva <amauribiarge@gmail.com>
2023-12-12 15:21:15 -0300

    Detect open text Editor + Read all content from de file opened

--- jas-plus-editor/extension.js
+++ jas-plus-editor/extension.js
@@ -1,35 +1,65 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "jas-plus-editor" is now active!');
+	console.log('Congratulations, your extension "AS Bundler" is now active!');
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('jas-plus-editor.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
+	let disposable = vscode.commands.registerCommand('jas-plus-editor.as-bundler', function () {
 
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from jas-plus editor!');
+		const activeEditor = vscode.window.activeTextEditor
+		const filePropertiesExtension = activeEditor.document.fileName.endsWith(".properties");
+		if (filePropertiesExtension) {
+
+			vscode.window.showInformationMessage('Hello World from AS Bundler!');
+
+			//const document = activeEditor.document;
+			var firstLine = activeEditor.document.lineAt(0);
+			var lastLine = activeEditor.document.lineAt(activeEditor.document.lineCount - 1);
+
+			
+			var myArray = [];
+			
+			const fullPropertiesContent = activeEditor.document.getText(new vscode.Range(firstLine.range.start, lastLine.range.end));
+			console.log(" Inside de IF fileTextToCursor @@@@@ #####################################: " + fullPropertiesContent.normalize);
+
+			fullPropertiesContent.normalize
+			console.log(typeof fullPropertiesContent);
+
+
+
+			const a = vscode.commands.executeCommand('editor.action.addSelectionToNextFindMatch');
+			
+			//const textLine = document.getWordRangeAtPosition();
+			const textLine = vscode.commands.executeCommand('editor.action.addSelectionToNextFindMatch');
+		}
 	});
 
-	context.subscriptions.push(disposable);
+	vscode.workspace.onDidChangeTextDocument(changeEvent => {
+		console.log(`Did change: ${changeEvent.document.uri}`);
+
+		for (const change of changeEvent.contentChanges) {
+			console.log(change.range); // range of text being replaced
+			console.log(change.text); // text replacement
+		}
+	});
+
+	//context.subscriptions.push(disposable);
 }
 
+async function test1() {
+	const saveFile = vscode.commands.executeCommand['workbench.action.files.save'];
+	if (saveFile) {
+
+	}
+}
 // This method is called when your extension is deactivated
 function deactivate() {
-	console.log('Amaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaauri')
+	console.log('deactivate')
 }
 
 module.exports = {
