10797dbe222a678eabe8e0309f9f8e2a15cab431
Amauri Silva <amauribiarge@gmail.com>
2024-01-16 05:32:02 -0300

    Clean the source and do comment in the code to refectory

--- jas-plus-editor/extension.js
+++ jas-plus-editor/extension.js
@@ -8,34 +8,22 @@ const vscode = require('vscode');
  */
 function activate(context) {
 
-	console.log('Congratulations, your extension "AS Bundler" is now active!');
+	console.log('The extension of "AS Bundler" was executed!');
 
-	let disposable = vscode.commands.registerCommand('jas-plus-editor.as-bundler', async function () {
+	vscode.commands.registerCommand('jas-plus-editor.as-bundler', async function () {
 
 		const activeEditor = vscode.window.activeTextEditor
 		const filePropertiesExtension = activeEditor.document.fileName.endsWith(".properties");
 		if (filePropertiesExtension) {
 
-			vscode.window.showInformationMessage('Hello World from AS Bundler!');
-
 			var firstLine = activeEditor.document.lineAt(0);
-			var lastLine = activeEditor.document.lineAt(activeEditor.document.lineCount - 1);
-
-
-
-			const fullPropertiesContent = activeEditor.document.getText(new vscode.Range(firstLine.range.start, lastLine.range.end));
-
-
-			//=====================================================================
 			var fullListOfParameters = [];
 			var finalListParameters = [];
-			var map = new Map();
-			let lastGroupOfParameter = "";
-
 			const size = activeEditor.document.lineCount;
 			var i = 0;
 
-			// Get all lines of the OpenTextEditor =====================================================================
+			// TODO ----------------------------------------------------------------------------------------------------------
+			// Get all lines of the OpenTextEditor
 			do {
 				var firstLine = activeEditor.document.lineAt(i);
 				const content = activeEditor.document.getText(new vscode.Range(firstLine.range.start, firstLine.range.end));
@@ -44,39 +32,44 @@ function activate(context) {
 				}
 				i++;
 			}
+			// END ----------------------------------------------------------------------------------------------------------
+
+
 			while (i < size)
-			//console.log("@@@@@@@@@@@@@@@@@@@@@@@@@@  "+ myArray[myArray.length -1]);
-			//console.log("WWWWWWWWWWWWWWWWWWWWWWWWW  "+ myArray);
-			//myArray2.sort();
-			//Get the group of each line/parameter
-			var mapFull = new Map();
+			var mapOfParameters = new Map();
 			var paramList = [];
-			var mapTwo = new Map();
+			var groupOfComments = new Map();
 
 			//Get all line of hastag and group them by catagory
-			mapTwo = hashtagRule(fullListOfParameters);
+			groupOfComments = hashtagRule(fullListOfParameters);
 
 			fullListOfParameters.sort();
 
+			// TODO ----------------------------------------------------------------------------------------------------------
 			var fullListOfParametersWithOutHastag = fullListOfParameters.filter(element => !element.startsWith("#"))
 
 			fullListOfParametersWithOutHastag.forEach((element) => {
 
 				const groupOfParameter = element.substring(0, element.indexOf("."));
 
-				if (mapFull.has(groupOfParameter)) {
+				if (mapOfParameters.has(groupOfParameter)) {
 
 					paramList.push(element);
-					mapFull.set(groupOfParameter, paramList);
+					mapOfParameters.set(groupOfParameter, paramList);
 
 				} else {
 					paramList = [];
 					paramList.push(element);
-					mapFull.set(groupOfParameter, paramList);
+					mapOfParameters.set(groupOfParameter, paramList);
 				}
 			});
 
-			mapFull.forEach((arrayParameter, key) => {
+			// END ----------------------------------------------------------------------------------------------------------
+
+
+
+			// TODO ----------------------------------------------------------------------------------------------------------
+			mapOfParameters.forEach((arrayParameter, key) => {
 
 				var currentGroupOfParam = [];
 				var currentIndexEquals = 0;
@@ -86,7 +79,6 @@ function activate(context) {
 				//get the array with size > 1
 				if (arrayParameter.length > 1) {
 					arrayParameter.forEach((element) => {
-						//console.log(" Inside de IF EEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEE "+ element + "  wwwwww   "+ key);
 
 						//get the intex of  =
 						currentIndexEquals = element.indexOf("=");
@@ -118,59 +110,50 @@ function activate(context) {
 					finalListParameters.push(paramFormatted);
 				}
 			});
+			// END ----------------------------------------------------------------------------------------------------------
 
+
+
+
+			// TODO ----------------------------------------------------------------------------------------------------------
 			var finalBlockOfParameters = "\n";
 			var hashTagtest = "";
 			var lastParam = "";
 			finalListParameters.sort();
 			finalListParameters.forEach((parameter) => {
-
-				console.log("WWWWWWWWWWWWWWWWWW " + parameter);
-
-				
-				// hashTagtest = addHashTagToParameter(parameter, mapTwo);
-				
-				var hashtagGroup = "";
 				var currentParam = parameter.substring(0, parameter.indexOf("."));
 				
 				if(lastParam.length > 0 && currentParam != lastParam){
 					finalBlockOfParameters += "\n";
 				}
 
-				mapTwo.forEach((arrayHashtag, key) => {
+				groupOfComments.forEach((arrayHashtag, key) => {
 					// finalBlockOfParameters += "\n"
 					if (key.includes(currentParam)) {
 						arrayHashtag.forEach((comment => {
 							finalBlockOfParameters += comment + "\n"
 						}))
-						mapTwo.delete(key);
+						groupOfComments.delete(key);
 					}
-					// finalBlockOfParameters += parameter + "\n"
 				});
-				
-				// console.log("ffffffffffffffffffff "+ hashtagGroup);
-				// console.log("AAAAA9999999999999 " + hashTagtest);
 				finalBlockOfParameters += parameter + "\n"
 				lastParam = currentParam;
 			});
+			// END ----------------------------------------------------------------------------------------------------------
+
+
+
 
 			// select all the text and run formatSelection
 			await vscode.commands.executeCommand('editor.action.selectAll');
 			await vscode.commands.executeCommand('editor.action.deleteLines');
 			await vscode.commands.executeCommand('cancelSelection')
-			// await vscode.commands.executeCommand('workbench.action.files.saveAll');
-
 
 			activeEditor.edit(async builder => {
 				builder.insert(new vscode.Position(0, 0), finalBlockOfParameters);
 
 				await vscode.commands.executeCommand('workbench.action.files.saveAll');
 			})
-
-
-
-
-
 		}
 	});
 }
